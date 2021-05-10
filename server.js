b436635b6c3a9d1ea1ed117645753f9eb644186c
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-05-10 15:56:17 -0400

    made changes to server.js and got hello world to display

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+app.get('/', (req,res)=>{
+  res.send('Hello World')
+})
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+  console.log('listening to port', PORT)
+})
